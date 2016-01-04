aae41eb6b6fa6661010adbb2e1df948a103b5aea
Santosh Gautam <santoshgautam7@gmail.com>
2016-01-04 15:54:28 -0800

    ui routes working.

--- client/app/app.js
+++ client/app/app.js
@@ -4,6 +4,18 @@
 angular.module('flickrViewer', [
   'ui.router'
 ])
+       .config(function ($stateProvider, $urlRouterProvider) {
+         $urlRouterProvider.otherwise('/');
+         $stateProvider
+           .state('pictures', {
+             url: '/pictures',
+             templateUrl: 'app/partials/pictures.html'
+           })
+           .state('comments', {
+             url: '/comments',
+             templateUrl: 'app/partials/comments.html'
+           })
+       })
 .controller('MainCtrl', function($scope) {
   $scope.hello = {};
   $scope.hello.hi = 'hi';
