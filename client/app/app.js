99aef1fe46ba8fca060f38f5e70b032546fdac7d
Santosh Gautam <santoshgautam7@gmail.com>
2016-01-04 17:46:41 -0800

    Fetching data from Flickr working.

--- client/app/app.js
+++ client/app/app.js
@@ -2,21 +2,31 @@
  * Created by santoshgautam on 1/4/16.
  */
 angular.module('flickrViewer', [
-  'ui.router'
+  'ui.router',
+  'ngResource'
 ])
-       .config(function ($stateProvider, $urlRouterProvider) {
-         $urlRouterProvider.otherwise('/');
-         $stateProvider
-           .state('pictures', {
-             url: '/pictures',
-             templateUrl: 'app/partials/pictures.html'
-           })
-           .state('comments', {
-             url: '/comments',
-             templateUrl: 'app/partials/comments.html'
-           })
-       })
-.controller('MainCtrl', function($scope) {
+.config(function ($stateProvider, $urlRouterProvider) {
+ $urlRouterProvider.otherwise('/');
+ $stateProvider
+   .state('pictures', {
+     url: '/pictures',
+     templateUrl: 'app/partials/pictures.html'
+   })
+   .state('comments', {
+     url: '/comments',
+     templateUrl: 'app/partials/comments.html'
+   })
+})
+.controller('MainCtrl', function($scope, $http) {
   $scope.hello = {};
   $scope.hello.hi = 'hi';
+  $http.jsonp('https://api.flickr.com/services/feeds/photos_public.gne?format=json')
+    .success(function(data) {
+
+    });
+    jsonFlickrFeed = function(data){
+      $scope.data = data;
+      console.log(data.items);
+    }
+
 });
